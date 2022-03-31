a18ca4f043221996c045e3d6de986414578afd79
Vanshaj Bhardwaj <79003511+vanshaj810@users.noreply.github.com>
2022-03-31 23:42:45 +0530

    Update Leaderboard.js

--- Module 2/Web Scraping/Leaderboard.js
+++ Module 2/Web Scraping/Leaderboard.js
@@ -3,6 +3,7 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/match-results";
 request(link, cb);
+let leaderboard = [];
 function cb(error, response, body) {
     if (error) {
         console.error("error:", error); // Print the error if one occurred
@@ -38,4 +39,34 @@ function cb2(error, response, body) {
 
         }
     }
+}
+processPlayer('Rohit', '15', '4', '2', '4');
+processPlayer('Virat', '50', '20', '4', '3')
+processPlayer('Rohit', '40', '20', '1', '2');
+console.log(leaderboard);
+function processPlayer(name, runs, balls, fours, sixes) {
+    runs = Number(runs);
+    balls = Number(balls);
+    fours = Number(fours);
+    sixes = Number(sixes);
+    for (let i = 0; i < leaderboard.length; i++) {
+        let playerObj = leaderboard[i];
+        if (playerObj.Name == name) {
+            //will do some work here
+            playerObj.Runs += runs;
+            playerObj.Balls += balls;
+            playerObj.Fours += fours;
+            playerObj.Sixes += sixes;
+            return;
+        }
+    }
+    // code coming here means we did not find our player inside leaderboard
+    let obj = {
+        Name: name,
+        Runs: runs,
+        Balls: balls,
+        Fours: fours,
+        Sixes: sixes
+    }
+    leaderboard.push(obj);
 }
