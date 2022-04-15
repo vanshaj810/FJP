be7a9a93dd40a9158063e7f91196b2445fc0c48a
Vanshaj Bhardwaj <79003511+vanshaj810@users.noreply.github.com>
2022-04-15 15:58:27 +0530

    done for the last class

--- Module 2/Web Scraping/Callbackk.js
+++ Module 2/Web Scraping/Callbackk.js
@@ -1,13 +1,20 @@
-const fs = require('fs');
-console.log("Before");
-fs.readFile("file.txt","utf-8",cb);
-function cb(error,data) {
-    if (error) {
+const fs = require("fs");
+
+console.log("Before ");
+
+fs.readFile("file.txt", function (error,data){
+    if(error){
         console.log(error);
-    } else {
-        console.log(data,"");
-        
+    }else{
+        console.log(data+"");
+        fs.readFile("f2.txt",function(error,data){
+            if(error){
+                console.log(error);
+            }else{
+                console.log(data+"");
+            }
+        });
     }
-}
-// console.log(content);
+});
+
 console.log("After");
